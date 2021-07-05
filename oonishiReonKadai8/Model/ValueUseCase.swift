f03b369f31637a1fee8cfc6065c42eee54952605
0429oonishi <oonishireon110@gmail.com>
2021-07-05 23:09:08 +0900

    ValueUseCase実装

--- oonishiReonKadai8/Model/ValueUseCase.swift
+++ oonishiReonKadai8/Model/ValueUseCase.swift
@@ -5,4 +5,20 @@
 //  Created by 大西玲音 on 2021/07/05.
 //
 
-import Foundation
+import RxSwift
+import RxRelay
+
+protocol ValueUseCaseProtocol {
+    var value: Observable<Float> { get }
+    func update(value: Float)
+}
+
+final class ValueUseCase: ValueUseCaseProtocol {
+    private let valueRelay = BehaviorRelay<Float>(value: 0)
+    var value: Observable<Float> {
+        valueRelay.asObservable()
+    }
+    func update(value: Float) {
+        valueRelay.accept(value)
+    }
+}
