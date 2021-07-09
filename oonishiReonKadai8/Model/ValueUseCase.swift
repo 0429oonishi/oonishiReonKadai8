31f19f9b16bdc997396342af787866906919fd27
0429oonishi <oonishireon110@gmail.com>
2021-07-09 22:10:20 +0900

    ValueUseCaseリファクタリング

--- oonishiReonKadai8/Model/ValueUseCase.swift
+++ oonishiReonKadai8/Model/ValueUseCase.swift
@@ -13,12 +13,16 @@ protocol ValueUseCaseProtocol {
     func update(value: Float)
 }
 
+// 値を変更でき、現在値を監視できる
 final class ValueUseCase: ValueUseCaseProtocol {
+    
     private let valueRelay = BehaviorRelay<Float>(value: 0)
     var value: Observable<Float> {
         valueRelay.asObservable()
     }
+    
     func update(value: Float) {
         valueRelay.accept(value)
     }
+    
 }
